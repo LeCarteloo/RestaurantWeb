fa5f56c40c656f6cff11ddd87800262496fb9b9e
LeCarteloo <filippapiernik1999@gmail.com>
2022-01-23 22:50:20 +0100

    Added hamburger, first view is partly responsive

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = () => {
+  const hamburger = document.querySelector(".hamburger");
+  const lines = document.querySelectorAll(".line");
+  const hamburgerBackground = document.querySelector(".hamburger-background");
+
+  hamburger.addEventListener("click", () => {
+    if (lines[0].classList.contains("active")) {
+      lines[0].classList.remove("active");
+      lines[1].classList.remove("unactive");
+      lines[2].classList.remove("active");
+      hamburgerBackground.classList.remove("active");
+      return;
+    }
+
+    lines[0].classList.add("active");
+    lines[1].classList.add("unactive");
+    lines[2].classList.add("active");
+    hamburgerBackground.classList.add("active");
+  });
+};

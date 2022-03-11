21b1672af3390fb9606c32e2622d41e01af3b1c1
LeCarteloo <filippapiernik1999@gmail.com>
2022-03-11 20:08:52 +0100

    Small menu item changes

--- swiper-menu.js
+++ swiper-menu.js
@@ -1,14 +1,17 @@
+const swiperElem = document.querySelector(".swiper");
+
+swiperElem.addEventListener("mousedown", () => {
+  console.log("YO");
+  swiperElem.style.cursor = "grabbing";
+});
+
+console.log(swiperElem);
+
 const swiper = new Swiper(".swiper", {
   loop: true,
   //   mousewheel: true,
   pagination: {
     el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
   },
 
   // And if we need scrollbar
